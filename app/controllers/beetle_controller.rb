f98ad3978dae7ecb32131321d96814ef1752a4d1
Jesús Burgos Maciá <jburmac@gmail.com>
2019-06-19 12:19:34 -0400

    Adds ability to spawn!

--- app/controllers/beetle_controller.rb
+++ app/controllers/beetle_controller.rb
@@ -1,7 +1,15 @@
 class BeetleController < ApplicationController
   def index
+    @beetles = Beetle.all
   end
 
-  def get
+  def create
+    Beetle.create!({
+      name: BeetleFactory.random_name,
+      favorite_food: "🌮",
+      rarity: "Yes"
+    })
+
+    redirect_to beetle_index_path
   end
 end
